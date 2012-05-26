9c55d611be05adc9165befa58703d7e78c9e8686
dmillett <dbmillett@gmail.com>
2012-05-26 15:16:40 -0500

    added more assertions to count file lines.

--- test/clash/test/core.clj
+++ test/clash/test/core.clj
@@ -1,15 +1,24 @@
 (ns clash.test.core
-   (:use [clash.core])
-   (:use [clojure.test]))
+  (:use [clash.core])
+  (:use [clojure.test]))
 
-(def tresource
-  (str (System/getProperty "user.dir") "/test/clash/test"))
+(use '[clojure.java.io :only(reader)])
 
+;; Test tools
 ;; todo: build macro for performance execution wrapping
 (defn milis
   [start]
   (* (- (System/nanoTime) start) (Math/pow 10.0 -6.0)))
 
+(defn count-file-lines
+  [file]
+  (with-open [rdr (reader file)]
+    (count (line-seq  rdr))) )
+
+(def tresource
+  (str (System/getProperty "user.dir") "/test/clash/test"))
+
+;; Tests
 (deftest test-str-contains
    (is (not (str-contains? nil "o")))
    (is (not (str-contains? "foo" nil)))
@@ -21,12 +30,15 @@
   (is (= 3 (count (prefix-command "foo|bar"))))
   (is (= 6 (count (prefix-command "foobar")))) )
 
-(def file1 (str tresource "/input1.txt"))
+;; Test simple grep
+(def input1 (str tresource "/input1.txt"))
 (def output1 (str tresource "/output1.txt"))
-(def command1 (str "grep message " file1))
+(def command1 (str "grep message " input1))
  
 (deftest test-jprocess-and-write
+  (is (= 4 (count-file-lines input1)))
   (let [t1 (System/nanoTime)]
     (is (nil? (jprocess-and-write command1 output1 "\n")))
-    (println "clojure + grep + decoded (" output1 ") Time(ms):" (milis t1))) )
+    (println "clojure + grep + decoded (" output1 ") Time(ms):" (milis t1)))
+  (is (= 3 (count-file-lines output1))) )
 
