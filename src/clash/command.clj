f941731519d1bf9b72d10819668e17d716d38393
dmillett <dbmillett@gmail.com>
2012-05-30 21:52:57 -0500

    renamed core.clj to command.clj, some cleanup and eclipse licensing

---
+++ src/clash/command.clj
@@ -0,0 +1,117 @@
+;   Copyright (c) David Millett. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns
+    ^{:author "David Millett"
+      :doc "Use performant shell commands like 'grep', 'cut',
+etc piped together on larger files."}
+    clash.command)
+
+(use '[clojure.java.io :only(reader writer)])
+(use '[clojure.string :only(split)])
+
+;; Linux/Unix "/bin/sh", "-c"
+;; Mac
+;; Windows: throw exception
+
+; Move to tools.clj
+(defn stb
+  "Sh*t the bed message."
+  [message]
+   (throw (RuntimeException. (str message))) )
+
+; Move to tools.clj
+(defn str-contains?
+  "Does a string contain a given text." 
+  [text, chars]
+  (if-not (or (empty? text) (empty? chars))
+          (. text contains chars)) )  
+
+;; todo: macro?
+(defn pipe
+  "Build a command array for linux, prefixing with the following
+  system commands: \"/bin/sh\", \"-c\", 'command'. This will
+  enable multiple commands to execute via 'pipe'. "
+  [command]
+  (if (str-contains? command "|")
+      ; linux, solaris, pretty much non microsoft
+      (into-array (list "/bin/sh" "-c" command))
+       command) )
+
+
+(defn jproc
+  "Get a Java Process for a Runtime system execution."
+  [command]
+  (let [updated (pipe command)]
+    (. (Runtime/getRuntime) exec updated)) )
+
+
+(defn jproc-instream
+  "Get the input stream for a Java Process."
+  [command]
+  (. (jproc command) getInputStream))
+
+ 
+(defn jproc-reader
+  "Get a clojure reader from a java InputStream."
+  [command]
+  (if-not (nil? command)
+    (reader (jproc-instream command))) )
+
+
+;; todo: make this into one method with writer or console dump??
+
+; It's pretty slow dumping to the console, but useful for testing.
+(defn jproc-dump
+  "Execution a shell system command, via java process, and
+  dump result to the console (slow). This is handled via
+  clojure reader to a line-seq. "
+  [command, delim]
+  (with-open [rdr (jproc-reader command)]
+    (doseq [line (line-seq rdr)]
+      (println (str line delim)))) )
+  
+(defn jproc-write
+  "Execute a System command, via java Process, and capture
+  the InputStream via clojure reader into a sequenc.e
+  Write the resulting output to a file (useful for grep)."
+  [command, output, delim]
+  (with-open [rdr (jproc-reader command)
+              wrt (writer output :append true)]
+    (doseq [line (line-seq rdr)]
+      (.write wrt (str line delim)))) )
+
+
+;; Explore using map and functions here??
+;; Explore using multiple functions with @
+(defmacro with-jproc
+  "A macro to combine clojure functions with a result
+  from a shell command. For example"
+  [command delim output function]
+  `(with-open [rdr# (jproc-reader ~command)
+               wrt# (writer ~output)]
+     (doseq [line# (line-seq rdr#)]
+       (let [result# (~function line#)]
+         (.write wrt# (str result# ~delim))))) )
+
+
+(defmacro with-jproc-dump
+  "Execute a sh command like grep and pass the result to
+  a function to work with. For example, pass the result
+  of a grep to 'last': (last (grep bar \"foobar\")) --> 'r'
+
+  Usage:
+  
+"
+  [command delim function]
+  `(with-open [rdr# (jproc-reader ~command)]
+     (doseq [line# (line-seq rdr#)]
+       (let [result# (~function line#)]
+         (println (str "macro result? " result# ~delim))))) )
+
+       
--- test/clash/test/command.clj
+++ test/clash/test/command.clj
@@ -1,5 +1,13 @@
-(ns clash.test.core
-  (:use [clash.core])
+;   Copyright (c) David Millett. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns clash.test.command
+  (:use [clash.command])
   (:use [clojure.test]))
 
 (use '[clojure.java.io :only(reader delete-file)])
@@ -54,10 +62,10 @@
      result#))
 
 (defn count-lines
-  "How many lines in a file?"
+  "How many lines in a small file?"
   [file]
   (with-open [rdr (reader file)]
-    (count (line-seq  rdr))) )
+    (count (line-seq rdr))) )
 
 (def tresource
   "Define the current test directory."
@@ -71,24 +79,54 @@
    (is (str-contains? "foo" "o"))
    (is (not (str-contains? "foo" "|"))) )
 
-(deftest test-prefix-command
-  (is (= 3 (count (prefix-command "foo|bar"))))
-  (is (= 6 (count (prefix-command "foobar")))) )
+(deftest test-pipe
+  (is (= 3 (count (pipe "foo|bar"))))
+  (is (= 6 (count (pipe "foobar"))))
+  )
 
 ;; Test simple grep
 (def input1 (str tresource "/input1.txt"))
 (def output1 (str tresource "/output1.txt"))
 (def command1 (str "grep message " input1))
 
+;; Commented out to reduce console spam
+(comment 
 (deftest test-jproc-dump
   (perf (jproc-dump command1 "") "console (dump) test"))
+)
 
-;; Using (nperf) instead of (time)
+;; Using (perf) instead of (time)
 (deftest test-jproc-write
   (is (= 4 (count-lines input1)))
-  (perf (jproc-write command1 output1 "\n") "Small file 'cl + grep' and dump")
+  (perf (jproc-write command1 output1 "\n") "'cl + grep' results to file")
   (is (= 3 (count-lines output1)))
-  ; cleanup
   (delete-file output1) )
 
-  
+(def command2 (str "grep message " input1 " | cut -d\",\" -f2 " input1))
+(def output2 (str tresource "/output2.txt"))
+
+(deftest test-jproc-write-grep-cut
+  (is (= 4 (count-lines input1)))
+  (perf (jproc-write command2 output2 ":") "'cl + grep + cut' results to file")
+  (is (= 1 (count-lines output2)))
+  ; To see results comment out the next line "is:this: hopefully: satire?" 
+  (delete-file output2)
+  )
+
+
+(def command3 (str "grep hopefully " input1))
+(def output3 (str tresource "/output3.txt"))
+
+(deftest test-with-jproc
+  (is (= 4 (count-lines input1)))
+  (with-jproc command3 "" output3 last)
+  (is (= 1 (count-lines output3)))
+  ; To see results comment out the next line "y"
+  (delete-file output3)
+  )
+
+;; Commented out to reduce console spam
+(comment
+(deftest test-with-jproc-dump
+  (with-jproc-dump command2 ":" last))
+)
