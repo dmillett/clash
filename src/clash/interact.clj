8ab4cbc9e77664e18e4afcf0c3cf01899263d12a
David Millett <dbmillett@gmail.com>
2013-05-05 21:14:10 -0500

    adding more test for interact.clj

--- src/clash/interact.clj
+++ src/clash/interact.clj
@@ -24,17 +24,65 @@
   to convert the text, and a structure to parse text into.
 
   Usage: (atomic-map-from-file \"log-file-foo.txt\"  "
-  [input predicate transform parse]
-  (let [result (atom {})]
-    (with-open [input_reader (reader input)]
-      ; Using 'doseq' releases the head of the file to avoid out of memory
-      (doseq [line (line-seq input_reader)]
-        (if (predicate line)
-          (let [tranformed (if-not (nil? transform) (transform line) line)
-                structure (parse tranformed)
-                key (System/nanoTime)
-                current (hash-map key structure)]
-            ; Merge the current structure into 'result' bypassing immutability
-            (swap! result merge current)) ))) result) )
+  ([input parser] (atomic-map-from-file input nil nil parser nil))
+  ([input predicate transformer parser key]
+    (let [result (atom {})]
+      (with-open [input_reader (reader input)]
+        ; Using 'doseq' releases the head of the file to avoid out of memory
+        (doseq [line (line-seq input_reader)]
+          (if (or (nil? predicate) (predicate line))
+            (let [tranformed (if-not (nil? transformer) (transformer line) line)
+                  structure (parser tranformed)
+                  k (if-not (nil? key) (key structure) (System/nanoTime))
+                  current (hash-map k structure)]
+              ; Merge the current structure into 'result' bypassing immutability
+              (if-not (nil? current) (swap! result merge current)) ) ))) result)) )
 
 
+(defn atomic-list-from-file
+  "Load ~structured text from a file into a list of data structures to
+  interact with at the command line (repl). Larger files and structures
+  may require increasing the jvm heap. It helps to have specific regex
+  to decrease the number of 'bad' structures included in the atomized data
+  structure.
+
+  Usage:
+  (atomic-list-from-file \"/foo.log\" foo-parser)
+  (atomic-list-from-file \"/foo.log\" is-foo? url-decode foo-parser)
+
+  'input' - a text file
+  'parser' - a function that parses a line of text (via regex) into a data structure
+  'predicates' - a function to include/exclude a line of text from parsing (see 'every-pred')
+  'transformer' - a function to alter the line (decode, decrypt, etc) prior to parsing
+
+   Alternatively, just the 2 arg function and rely on the parser to perform much of
+   the functionality with more strict regex."
+  ([input parser] (atomic-list-from-file input nil nil parser))
+  ([input predicate transformer parser]
+    (let [result (atom '())]
+      (with-open [input_reader (reader input)]
+        (doseq [line (line-seq input_reader)]
+          (if (or (nil? predicate) (predicate line))
+            (let [transformed (if-not (nil? transformer) (transformer line) line)
+                  data (parser transformed)]
+              ; Updating 'result' with 'data'
+              (if-not (nil? data) (swap! result conj data)) )) )) result) ) )
+
+;;
+;; To pass along more than one condition, use (every-pred p1 p2 p3)
+;; Example: (def even3 (every-pred even? #(mod % 3)))
+(defn count-with-conditions
+  "Perform a count on each data structure in a list if it matches
+  the conditions defined in the predicates function. The predicates
+  function may contain multiple conditions when used with (every-pred p1 p2)."
+  [count solutions predicates]
+  (reduce (fn [count solution]
+            (if (predicates solution)
+              (inc count)
+              count
+              ))
+    0 solutions) )
+
+;(defn collect-with-condition
+;
+;  )
--- test/clash/interact_test.clj
+++ test/clash/interact_test.clj
@@ -7,10 +7,11 @@
 ;   You must not remove this notice, or any other, from this software.
 
 (ns clash.interact_test
+  (:require [clash.text_tools :as tt])
   (:use [clojure.test]
         [clash.interact]
-        [clash.text_tools]
-        [clash.command_test]) )
+        [clash.command_test]
+        [clash.tools]) )
 
 
 (def simple-file (str tresource "/simple-structured.log"))
@@ -19,22 +20,48 @@
 
 (defn simple-stock-message-parser
   [line]
-  (let [splitsky (split-with-regex line #"\|")
+  (let [splitsky (tt/split-with-regex line #"\|")
         date (first splitsky)
         message (last splitsky)
         corrected (str date "," message)]
-    (text-structure-to-map corrected #"," simple-stock-structure)) )
+    (tt/text-structure-to-map corrected #"," simple-stock-structure)) )
+
+; 05042013-13:24:12.000|sample-server|1.0.0|info|Buy,FOO,500,12.00
+(def detailed-stock-pattern #"(\d{8}-\d{2}:\d{2}:\d{2}.\d{3})\|.*\|(\w*),(\w*),(\d*),(.*)")
+
+(defn better-stock-message-parser
+  [line]
+  (tt/regex-group-into-map line simple-stock-structure detailed-stock-pattern) )
 
 (defn is-buy-or-sell?
   [line]
-  (if (or (str-contains? line "Buy") (str-contains? line "Sell"))
+  (if (or (tt/str-contains? line "Buy") (tt/str-contains? line "Sell"))
     true
     false) )
 
 
 (deftest test-atomic-map-from-file
-  (let [result (atomic-map-from-file simple-file is-buy-or-sell? nil simple-stock-message-parser)]
-    ;(println @result)
-    (is (= 6 (count @result)))
+  (let [result (latency (atomic-map-from-file simple-file is-buy-or-sell? nil simple-stock-message-parser nil)
+                     "map latency")
+        data (deref (-> result :result))]
+    ;(println (-> result :latency_text))
+    (is (= 6 (count data)))
     ) )
 
+(deftest test-atomic-list-from-file
+  (let [result1 (latency (atomic-list-from-file simple-file is-buy-or-sell? nil simple-stock-message-parser)
+                 "list latency")
+        data1 (deref (-> result1 :result))
+        result2 (atomic-list-from-file simple-file simple-stock-message-parser)]
+
+    (is (= 6 (count data1)))
+    ) )
+
+;; Note the count is 8 instead of 6 because the 'parser' function is more specific
+(deftest test-atomic-list-from-fil__2_parameters_better_parser
+  (let [result1 (atomic-list-from-file simple-file simple-stock-message-parser)
+        result2 (atomic-list-from-file simple-file better-stock-message-parser)]
+
+    (is (= 8 (count @result1)))
+    (is (= 6 (count @result2)))
+    ) )
