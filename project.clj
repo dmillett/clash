7719108ea9a3bb5c05d4362426cc57a0e84a5a96
David Millett <dbmillett@gmail.com>
2013-09-09 21:00:48 -0500

    removing aot from project so leinengen 2.3.2 does not freak out.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "A clojure library that encapsulates shell functionality for commands
   like 'grep' and 'cut'. This is useful for search or indexing large text files."
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :aot [clash]
   :jvm-opts ["-Xms256m" "-Xmx512m"]
   :repl-options {:init (do
                          (load-file "src/clash/tools.clj")
